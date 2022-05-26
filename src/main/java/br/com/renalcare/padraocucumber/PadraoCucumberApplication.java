415f9dce27c1f2c7b554a990a92eaa67b557236a
Lucas Yamaguti <lucasycosta@gmail.com>
2022-05-26 11:06:48 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/renalcare/padraocucumber/PadraoCucumberApplication.java
@@ -0,0 +1,13 @@
+package br.com.renalcare.padraocucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoCucumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoCucumberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/renalcare/padraocucumber/PadraoCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renalcare.padraocucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadraoCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
